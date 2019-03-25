4a99c1b5639235543159b8a2c7df1f262bf60813
陈子繁 <chenzf@pvc123.com>
2019-03-25 10:57:04 +0800

    rpc for hao.pvc123.com 注册

--- src/rpc/RpcClient.php
+++ src/rpc/RpcClient.php
@@ -11,34 +11,117 @@ namespace MondaMQ\rpc;
 
 
 class RpcClient {
+
     private $host;
     private $port;
+    private $interface;
+    private $version;
+    private $method;
+    private $params;
 
+    /**
+     * @return mixed
+     */
+    public function getHost() {
+        return $this->host;
+    }
 
-    public function __construct($host, $port) {
+    /**
+     * @param mixed $host
+     */
+    public function setHost($host) {
         $this->host = $host;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPort() {
+        return $this->port;
+    }
+
+    /**
+     * @param mixed $port
+     */
+    public function setPort($port) {
         $this->port = $port;
     }
 
     /**
-     * @param string $interface
-     * @param string $version
-     * @param string $method
-     * @param array $params
-     * @return bool|string
+     * @return mixed
+     */
+    public function getInterface() {
+        return $this->interface;
+    }
+
+    /**
+     * @param mixed $interface
+     */
+    public function setInterface($interface) {
+        $this->interface = $interface;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVersion() {
+        return $this->version;
+    }
+
+    /**
+     * @param mixed $version
+     */
+    public function setVersion($version) {
+        $this->version = $version;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMethod() {
+        return $this->method;
+    }
+
+    /**
+     * @param mixed $method
+     */
+    public function setMethod($method) {
+        $this->method = $method;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParams() {
+        return $this->params;
+    }
+
+    /**
+     * @param mixed $params
+     */
+    public function setParams(array $params) {
+        $this->params = $params;
+    }
+
+
+    /**
+     * @return bool
      * @throws \Exception
      * 远程调用
      */
-    public function call(string $interface, string $version, string $method, array $params = []) {
+    public function call() {
+        if (!empty($this->interface) || empty($this->version) || empty($this->version) || empty($this->host) || empty($this->port)) {
+            throw new \Exception("rpc配置出错");
+        }
         $fp = stream_socket_client("tcp://{$this->host}:{$this->port}", $errno, $errstr);
         if (!$fp) {
             throw new \Exception("stream_socket_client fail errno={$errno} errstr={$errstr}");
         }
         $data = [
-            'interface' => $interface,
-            'version' => $version,
-            'method' => $method,
-            'params' => $params,
+            'interface' => $this->interface,
+            'version' => $this->version,
+            'method' => $this->method,
+            'params' => $this->params,
             'logid' => uniqid(),
             'spanid' => 0,
         ];
@@ -46,8 +129,8 @@ class RpcClient {
         fwrite($fp, $data);
         $result = fread($fp, 1024);
         fclose($fp);
-         $result = json_decode($result,1);
-        if(isset($result['status']) && isset($result['data']) && $result['status'] == 200){
+        $result = json_decode($result, 1);
+        if (isset($result['status']) && isset($result['data']) && $result['status'] == 200) {
             return $result['data'];
         }
         //记录日志
